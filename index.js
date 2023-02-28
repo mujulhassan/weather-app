02a2a1bcda18b7b3b4644e36674a7c8d3df9bfeb
mujulhassan <mujulhassan@gmail.com>
2023-02-28 02:31:34 +0000

    Updated index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const { response } = require("express");
+
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404= document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKEY = 'b3e0aa89ac0d4fe023289e8b98c476f3';
+    const city = document.querySelector('.search-box input').value;
+ 
+    if(city === '')
+        return;
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json =>{
+
+            if(json.cod === '404'){
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'npne';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+
+
+
+        })
+
+})
